0df22f741c5c837e1ff784ed85de5ed649b85775
firmanwibowo <firmanwibowo77@gmail.com>
2026-04-27 10:42:17 +0700

    Master Position

---
+++ ESKA_APP/ESKA_APP/Controllers/Master/Position/PositionController.cs
@@ -0,0 +1,132 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using DevExpress.Web.Mvc;
+using Models.Master.Position;
+
+namespace Controllers.Master
+{
+    public partial class PositionController : BaseController
+    {
+        string VIEW_DETAIL = "Position";
+        string VIEW_FORM_PARTIAL = "Partial/Position_Form_Partial";
+        string VIEW_LIST_PARTIAL = "Partial/Position_List_Partial";
+        string VIEW_PANEL_LIST_PARTIAL = "Partial/Position_Panel_List_Partial";
+
+
+        PositionService positionService;
+
+        public ActionResult Index()
+        {
+            return RedirectToAction("Detail");
+        }
+
+        public ActionResult Detail(long Id = 0)
+        {
+            int userId = (int)Session["userId"];
+
+
+            positionService = new PositionService();
+            PositionModel positionModel;
+            if (Id == 0)
+            {
+                ViewBag.initNew = true;
+                positionModel = positionService.GetNewModel(userId);
+                positionModel.IsActive = "Y";
+                positionModel._FormMode = FormModeEnum.New;
+            }
+            else
+            {
+                positionService = new PositionService();
+                positionModel = positionService.GetById(userId, Id);
+                positionModel._FormMode = FormModeEnum.Edit;
+            }
+
+            return View(VIEW_DETAIL, positionModel);
+        }
+
+        public ActionResult DetailPartial(long Id = 0, string copyFromForm = "", long copyFromId = 0)
+        {
+            int userId = (int)Session["userId"];
+
+
+            PositionModel positionModel;
+
+            positionService = new PositionService();
+            if (Id == 0)
+            {
+                positionModel = positionService.GetNewModel(userId);
+                positionModel._FormMode = FormModeEnum.New;
+            }
+            else
+            {
+                positionModel = positionService.GetById(userId, Id);
+                if (positionModel != null)
+                {
+                    positionModel._FormMode = FormModeEnum.Edit;
+                }
+                else
+                {
+                    positionModel = positionService.GetNewModel(userId);
+                    positionModel._FormMode = FormModeEnum.New;
+                }
+            }
+
+            return PartialView(VIEW_FORM_PARTIAL, positionModel);
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult Add([ModelBinder(typeof(DevExpressEditorsBinder))]  PositionModel PositionModel)
+        {
+            int userId = (int)Session["userId"];
+
+            PositionModel._UserId = (int)Session["userId"];
+            positionService = new PositionService();
+
+            if (ModelState.IsValid)
+            {
+                long Id = 0;
+
+                Id = positionService.Add(PositionModel);
+                PositionModel = positionService.GetById(userId, Id);
+                PositionModel._FormMode = Models.FormModeEnum.Edit;
+            }
+            else
+            {
+                string message = GetErrorModel();
+                throw new Exception(string.Format("[VALIDATION] {0}", message));
+            }
+
+            return PartialView(VIEW_FORM_PARTIAL, PositionModel);
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult Update([ModelBinder(typeof(DevExpressEditorsBinder))]  PositionModel positionModel)
+        {
+            int userId = (int)Session["userId"];
+
+            positionModel._UserId = (int)Session["userId"];
+            positionService = new PositionService();
+            positionModel._FormMode = FormModeEnum.Edit;
+
+
+
+            //if (ModelState.IsValid)
+            //{
+            positionService.Update(positionModel);
+            positionModel = positionService.GetById(userId, positionModel.Id);
+            //}
+            //else
+            //{
+            //    string message = GetErrorModel();
+
+            //    throw new Exception(string.Format("[VALIDATION] {0}", message));
+            //}
+
+            return PartialView(VIEW_FORM_PARTIAL, positionModel);
+        }
+
+    }
+}
---
+++ ESKA_DI/Models/Master/Position/Position__List_Model.cs
@@ -0,0 +1,241 @@
+﻿using DevExpress.Data;
+using DevExpress.Data.Filtering;
+using DevExpress.Data.Linq;
+using DevExpress.Data.Linq.Helpers;
+using DevExpress.Web.Mvc;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Models;
+using Models._Utils;
+using ESKA_DI.Models._EF;
+using Models._Ef;
+
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+
+namespace Models.Master.Position
+{
+    public class ListFindParamPosition
+    {
+        public bool IsFindTransDate { get; set; }
+        public DateTime? TransDate_From { get; set; }
+        public DateTime? TransDate_To { get; set; }
+    }
+
+    public class PositionView___
+    {
+        public long Id { get; set; }
+
+        public string PositionName { get; set; }
+
+        public string IsActive { get; set; }
+
+    }
+
+    public class Position__List_Model
+    {
+        static string ViewSql = "SELECT *" +
+                                "FROM \"Tm_Position\" T0 " +
+                                "ORDER BY T0.\"CreatedDate\" DESC";
+
+        public static void SetBindingData(GridViewModel state, int userId, ListFindParamPosition cflParam)
+        {
+            string sqlCriteria = GetSqlFromGridViewModelState.getHanaCriteria(state);
+            string sqlSort = GetSqlFromGridViewModelState.getHanaSort(state);
+
+            using (var CONTEXT = new HANA_APP())
+            {
+                var formAuthorizeSqlWhere = GeneralGetList.GetFormTransAuthorizeSqlWhere(CONTEXT, userId, "Position");
+                if (!string.IsNullOrEmpty(formAuthorizeSqlWhere))
+                {
+                    if (string.IsNullOrEmpty(sqlCriteria))
+                    {
+                        sqlCriteria = formAuthorizeSqlWhere;
+                    }
+                    else
+                    {
+                        sqlCriteria = sqlCriteria + " AND " + formAuthorizeSqlWhere;
+                    }
+
+                }
+
+                var dataRowCount = GetRowCount(CONTEXT, userId, cflParam, sqlCriteria);
+                var dataList = GetDataList(CONTEXT, userId, cflParam, sqlCriteria, sqlSort, state.Pager.PageIndex, state.Pager.PageSize);
+
+                state.ProcessCustomBinding(
+                  new GridViewCustomBindingGetDataRowCountHandler(args =>
+                  {
+                      GetDataRowCount(args, dataRowCount);
+                  }),
+                  new GridViewCustomBindingGetDataHandler(args =>
+                  {
+                      GetData(args, dataList);
+                  })
+              );
+            }
+        }
+
+        public static void GetDataRowCount(GridViewCustomBindingGetDataRowCountArgs e, int dataRowCount)
+        {
+            e.DataRowCount = dataRowCount;
+        }
+
+        public static void GetData(GridViewCustomBindingGetDataArgs e, List<PositionView___> dataList)
+        {
+            e.Data = dataList;
+        }
+
+        public static int GetRowCount(HANA_APP CONTEXT, int userId, ListFindParamPosition param, string sqlCriteria)
+        {
+
+            if (sqlCriteria == null)
+            {
+                sqlCriteria = "";
+            }
+            if (sqlCriteria != "")
+            {
+                //sqlCriteria = " AND ( " + sqlCriteria + " )";
+                sqlCriteria = " AND  " + sqlCriteria;
+            }
+
+
+
+            int dataRowCount = 0;
+            string ssql = " ";
+            ssql = "SELECT COUNT(*) AS IDU FROM (" + ViewSql + ") T0  WHERE 1=1 " + sqlCriteria;
+
+            if (param != null)
+            {
+                if (param.IsFindTransDate)
+                {
+                    if ((param.TransDate_From != null) && (param.TransDate_To != null))
+                    {
+                        //ssql = ssql + " AND \"TransDate\">=:p0 AND \"TransDate\"<=:p1 ";
+                        dataRowCount = CONTEXT.Database.SqlQuery<int>(ssql, param.TransDate_From.Value.Date, param.TransDate_To.Value.Date).FirstOrDefault<int>();
+                    }
+                    else if (param.TransDate_From != null)
+                    {
+                        //ssql = ssql + " AND \"TransDate\">=:p0 ";
+                        dataRowCount = CONTEXT.Database.SqlQuery<int>(ssql, param.TransDate_From.Value.Date).FirstOrDefault<int>();
+                    }
+                    else if (param.TransDate_To != null)
+                    {
+                        //ssql = ssql + " AND \"TransDate\"<=:p0 ";
+                        dataRowCount = CONTEXT.Database.SqlQuery<int>(ssql, param.TransDate_To.Value.Date).FirstOrDefault<int>();
+                    }
+                }
+                else
+                {
+                    dataRowCount = CONTEXT.Database.SqlQuery<int>(ssql).FirstOrDefault<int>();
+                }
+            }
+            else
+            {
+                dataRowCount = CONTEXT.Database.SqlQuery<int>(ssql).FirstOrDefault<int>();
+            }
+
+            return dataRowCount;
+        }
+
+        public static List<PositionView___> GetDataList(HANA_APP CONTEXT, int userId, ListFindParamPosition param, string sqlCriteria, string sqlSort, int PageIndex, int PageSize)
+        {
+
+
+            if (sqlCriteria == null)
+            {
+                sqlCriteria = "";
+            }
+            if (sqlSort == null)
+            {
+                sqlSort = "";
+            }
+
+            if (sqlCriteria != "")
+            {
+                sqlCriteria = " AND (" + sqlCriteria + ")";
+            }
+
+
+            if (sqlSort == "")
+            {
+                sqlSort = " ORDER BY \"CreatedDate\" DESC ";
+            }
+
+            var views = new List<PositionView___>();
+
+            string ssql = "";
+            ssql = "SELECT T0.* FROM (" + ViewSql + ") T0  WHERE 1=1 " + sqlCriteria;
+            string ssqlLimit = string.Format(" LIMIT {0} OFFSET {1} ", PageSize, (PageIndex) * PageSize);
+
+
+
+            if (param != null)
+            {
+                if (param.IsFindTransDate)
+                {
+                    if ((param.TransDate_From != null) && (param.TransDate_To != null))
+                    {
+                        //ssql = ssql + " AND \"TransDate\">=:p0 AND \"TransDate\"<=:p1 ";
+                        views = CONTEXT.Database.SqlQuery<PositionView___>(ssql + sqlSort + ssqlLimit, param.TransDate_From.Value.Date, param.TransDate_To.Value.Date).ToList();
+                    }
+                    else if (param.TransDate_From != null)
+                    {
+                        //ssql = ssql + " AND \"TransDate\">=:p0 ";
+                        views = CONTEXT.Database.SqlQuery<PositionView___>(ssql + sqlSort + ssqlLimit, param.TransDate_From.Value.Date).ToList();
+                    }
+                    else if (param.TransDate_To != null)
+                    {
+                        //ssql = ssql + " AND \"TransDate\"<=:p0 ";
+                        views = CONTEXT.Database.SqlQuery<PositionView___>(ssql + sqlSort + ssqlLimit, param.TransDate_To.Value.Date).ToList();
+                    }
+                }
+                else
+                {
+                    views = CONTEXT.Database.SqlQuery<PositionView___>(ssql + sqlSort + ssqlLimit).ToList();
+                }
+            }
+            else
+            {
+                views = CONTEXT.Database.SqlQuery<PositionView___>(ssql + sqlSort + ssqlLimit).ToList();
+            }
+
+            if (views.Count == 0)
+            {
+                PositionView___ view = new PositionView___();
+                views.Add(view);
+            }
+
+            return views;
+
+        }
+
+
+        public static GridViewModel CreateGridViewModel()
+        {
+            var viewModel = new GridViewModel();
+
+
+            return viewModel;
+        }
+
+
+        public static GridViewSettings CreateExportGridViewSettings()
+        {
+
+            GridViewSettings settings = new GridViewSettings();
+            settings.Name = "List Dokumen";
+
+            settings.KeyFieldName = "Id";
+            settings.Columns.Add("Id").Visible = false;
+            settings.Columns.Add("PositionName", "Position Name");
+            settings.Columns.Add("IsActive", "Is Active");
+            return settings;
+        }
+
+    }
+
+
+}
