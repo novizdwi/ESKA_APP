78a1f13777d93e809d67b63328ba304f35f7aa2d
Noviz <Noviz@RDPHANA>
2026-06-29 10:58:47 +0700

    ad HNcode, OperatorName to post to SAP

--- ESKA_DI/Models/_EF/HANA_APP.Context.cs
+++ ESKA_DI/Models/_EF/HANA_APP.Context.cs
@@ -81,15 +81,23 @@ namespace ESKA_DI.Models._EF
         public virtual DbSet<Tx_GoodsReceiptPO_Item> Tx_GoodsReceiptPO_Item { get; set; }
         public virtual DbSet<Tx_GoodsReceiptPO_Item_Batch> Tx_GoodsReceiptPO_Item_Batch { get; set; }
         public virtual DbSet<Tx_GoodsReceiptPO_Item_Batch_Scale> Tx_GoodsReceiptPO_Item_Batch_Scale { get; set; }
-        public virtual DbSet<Tp_UserAlert> Tp_UserAlert { get; set; }
+        public virtual DbSet<Tx_IssueAndReceipt> Tx_IssueAndReceipt { get; set; }
+        public virtual DbSet<Tx_IssueAndReceipt_Approval> Tx_IssueAndReceipt_Approval { get; set; }
+        public virtual DbSet<Tx_IssueAndReceipt_Issue_Item> Tx_IssueAndReceipt_Issue_Item { get; set; }
+        public virtual DbSet<Tx_IssueAndReceipt_Issue_Item_Batch> Tx_IssueAndReceipt_Issue_Item_Batch { get; set; }
+        public virtual DbSet<Tx_IssueAndReceipt_Issue_Item_Batch_Scale> Tx_IssueAndReceipt_Issue_Item_Batch_Scale { get; set; }
+        public virtual DbSet<Tx_IssueAndReceipt_Receipt_Item> Tx_IssueAndReceipt_Receipt_Item { get; set; }
+        public virtual DbSet<Tx_IssueAndReceipt_Receipt_Item_Batch> Tx_IssueAndReceipt_Receipt_Item_Batch { get; set; }
+        public virtual DbSet<Tx_IssueAndReceipt_Receipt_Item_Batch_Scale> Tx_IssueAndReceipt_Receipt_Item_Batch_Scale { get; set; }
+        public virtual DbSet<Tx_ProcessCard> Tx_ProcessCard { get; set; }
         public virtual DbSet<Tx_ProcessCard_Approval> Tx_ProcessCard_Approval { get; set; }
         public virtual DbSet<Tx_StockOpname> Tx_StockOpname { get; set; }
         public virtual DbSet<Tx_StockOpname_Approval> Tx_StockOpname_Approval { get; set; }
         public virtual DbSet<Tx_StockOpname_Item> Tx_StockOpname_Item { get; set; }
         public virtual DbSet<Tx_StockOpname_Item_Batch> Tx_StockOpname_Item_Batch { get; set; }
         public virtual DbSet<Tx_StockOpname_Item_Batch_Scale> Tx_StockOpname_Item_Batch_Scale { get; set; }
-        public virtual DbSet<Tx_ProcessCard> Tx_ProcessCard { get; set; }
-        public virtual DbSet<Tx_ProcessCard_Detail> Tx_ProcessCard_Detail { get; set; }
         public virtual DbSet<WeighbridgeTransactions> WeighbridgeTransactions { get; set; }
+        public virtual DbSet<Tp_UserAlert> Tp_UserAlert { get; set; }
+        public virtual DbSet<Tx_ProcessCard_Detail> Tx_ProcessCard_Detail { get; set; }
     }
 }
--- ESKA_DI/Models/_EF/Tx_IssueAndReceipt_Receipt_Item_Batch_Scale.cs
+++ ESKA_DI/Models/_EF/Tx_IssueAndReceipt_Receipt_Item_Batch_Scale.cs
@@ -12,24 +12,15 @@ namespace ESKA_DI.Models._EF
     using System;
     using System.Collections.Generic;
     
-    public partial class Tx_GoodsReceiptPO_Item
+    public partial class Tx_IssueAndReceipt_Receipt_Item_Batch_Scale
     {
-        public long DetId { get; set; }
-        public Nullable<long> Id { get; set; }
-        public string ItemCode { get; set; }
-        public string ItemName { get; set; }
+        public long DetDetDetId { get; set; }
+        public Nullable<long> DetDetId { get; set; }
         public Nullable<int> Quantity { get; set; }
-        public Nullable<int> QuantityCreated { get; set; }
         public string Uom { get; set; }
-        public Nullable<int> UomEntry { get; set; }
-        public Nullable<decimal> UnitPrice { get; set; }
-        public string Whse { get; set; }
         public Nullable<decimal> Netto { get; set; }
-        public string Department { get; set; }
         public Nullable<int> LineNum { get; set; }
         public string LineStatus { get; set; }
-        public Nullable<long> BaseEntry { get; set; }
-        public Nullable<int> BaseLine { get; set; }
         public Nullable<System.DateTime> CreatedDate { get; set; }
         public Nullable<System.DateTime> ModifiedDate { get; set; }
         public Nullable<int> CreatedUser { get; set; }
