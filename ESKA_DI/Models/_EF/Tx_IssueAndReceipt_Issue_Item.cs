32c3e0e03d3b2803c33c2825f31b6c835a054e12
Noviz <Noviz@RDPHANA>
2026-08-10 10:18:45 +0700

    ...

--- ESKA_DI/Models/_EF/Tx_IssueAndReceipt_Issue_Item.cs
+++ ESKA_DI/Models/_EF/Tx_IssueAndReceipt_Issue_Item.cs
@@ -33,7 +33,6 @@ namespace ESKA_DI.Models._EF
         public Nullable<System.DateTime> CreatedDate { get; set; }
         public Nullable<System.DateTime> ModifiedDate { get; set; }
         public Nullable<int> CreatedUser { get; set; }
-        public Nullable<int> ModifiedUser { get; set; }
-        public Nullable<int> QuantityCreated { get; set; }
+        public Nullable<int> ModifiedUser { get; set; } 
     }
 }
